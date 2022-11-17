42696e91b184949f1a0edab754421dfcbc5d5686
k-minkeon <117174026+k-minkeon@users.noreply.github.com>
2022-11-17 10:05:22 +0900

    qweq

--- module/def_mg.py
+++ module/def_mg.py
@@ -1,35 +1,86 @@
-from module.dir import dir_min_open, dir_day_open, dir_min_update_open
-from module.dir import dir_day_update_open, dir_concat_open
+from module import dir
+from module.dir import dir_min_open, dir_day_open
+from module.dir import dir_min_update_open, dir_day_update_open, dir_concat_open
 from module.dir import dir_min_update_save, dir_day_update_save
-from module.dir import dir_concat_save,dir_label_save
-from module.dir import file_min, file_day, file_min_update, file_day_update, file_concat
+from module.dir import dir_concat_save
+from module.dir import dir_label_save
+from module.dir import file_min, file_day
+from module.dir import file_min_update, file_day_update, file_concat
 
 from module.setting import instCpCybos
-from module.search import search_by_code
-from module.get import get_stock_info
-from module.update import update_stock_list
 
+ 
 from tqdm import tqdm
 from datetime import datetime
 from datetime import timedelta
 import pandas as pd
 import time
-import os
 
 
-# 파일 경로 설정 후 오픈
-def file_open(file_open):
-    file_min = os.listdir(f'{file_open}')
-    file_min.sort()
-    
-    return file_min
+# 코스피 코스닥(분봉) 업데이트
+def update_min_stock_info():
+    for file_name in file_min:
+        stock_code = file_name.split('_')[-2]
+        temp_df = pd.read_csv(f'{dir_min_open}{file_name}', index_col='Unnamed: 0', encoding='utf-8 sig')
+        
+        last_day = str(temp_df.index[0]) # 20221031
 
+        day_format = '%Y%m%d'
+        plus_day = timedelta(days=1)
+
+        transfer_last_day = datetime.strptime(last_day, day_format) # 20221031
+        start_day = datetime.strftime(transfer_last_day + plus_day, day_format) # 20221101
+
+        today = str(datetime.today().year) + str(datetime.today().month) + str(datetime.today().day) # 20221116
+
+        
+
+        while True:
+
+            instStockChart.SetInputValue(0, 'A'+stock_code) # 종목명
+            instStockChart.SetInputValue(1, ord('1')) # 1 : 기간으로 요청, 2: 개수로 요청
+            instStockChart.SetInputValue(3, start_day) # 요청 시작일
+            instStockChart.SetInputValue(2, start_day) # 요청 종료일
+            instStockChart.SetInputValue(5, [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 13, 14, 15, 16, 17,18, 19,20,21,22,23,24,25,26])
+            instStockChart.SetInputValue(6, ord('m')) # 'D' : 일봉, 'm' : 분봉
+            instStockChart.SetInputValue(9, ord('1'))
+
+            instStockChart.BlockRequest() # 위 정보로 요청
+
+            numrow, numcolumn = instStockChart.GetHeaderValue(3), instStockChart.GetHeaderValue(2)
+
+            index = []
+            for i in range(numrow):
+                index_ = str(instStockChart.GetDataValue(0,i))
+                index.append(index_)
+
+            stock_info = pd.DataFrame(columns=numcolumn[1:], index=index)
+
+            for num in range(numrow):
+                for col in range(len(numcolumn)):
+                    # 1,2,3,4,5,6,7,8,9, 10
+                    stock_info.iloc[num, col-1] = str(instStockChart.GetDataValue(col,num))
+
+            update_min_df = pd.concat([stock_info, temp_df])
+            
+            
+            start_day = datetime.strftime(datetime.strptime(start_day, day_format) + timedelta(days=1), day_format)
+            transfer_end_day = datetime.strftime(datetime.strptime(today, day_format) + timedelta(days=1), day_format) # 20221116
+            time.sleep(uniform(0.15, 0.3))
+
+            if instCpCybos.GetLimitRemainCount(1) < 3:
+                time.sleep(10)
+
+            if start_day == transfer_end_day:
+                update_min_df.to_csv(f'{dir_min_update_save}{file_name}', encoding='utf-8 sig')
+                break
+
+    return update_min_df
 
 
 
 # 코스피 코스닥(일봉) 업데이트
-def update_day_stock_info(file_day, dir_day_open, dir_day_update_save):
-    
+def update_day_stock_info():
     for file_name in tqdm(file_day):
         temp_df = pd.read_csv(f'{dir_day_open}{file_name}', index_col='Unnamed: 0', encoding='utf-8 sig')
         
@@ -70,30 +121,31 @@ def update_day_stock_info(file_day, dir_day_open, dir_day_update_save):
     return update_day_df
 
 
-
 # 분봉 일봉 합치기
-def save_min_day_concat_param(file_min_update, file_day_update, dir_min_update, dir_day_update, dir_concat_save):
+def save_min_day_concat_info():
     for min_df, day_df in zip(file_min_update, file_day_update):
-        stock_min_df = pd.read_csv(f'{dir_min_update}{min_df}', index_col='Unnamed: 0')
-        stock_day_df = pd.read_csv(f'{dir_day_update}{day_df}', index_col='Unnamed: 0')
+        stock_min_df = pd.read_csv(f'{dir_min_update_open}{min_df}')
+        stock_day_df = pd.read_csv(f'{dir_day_update_open}{day_df}')
         
-        concat_list = []
-        concat_list.append('A' + min_df.split('_')[-2])
-        
-        for v in tqdm(stock_min_df.index):
-            input_list = ['전일대비','상장주식수','시가총액','외국인주문한도수량'
+        input_list = ['전일대비','상장주식수','시가총액','외국인주문한도수량'
                     ,'외국인주문가능수량','외국인현보유수량','외국인현보유비율'
-                    ,'수정주가일자','수정주가비율','기관순매수량','기관누적순매수량']
+                    ,'수정주가일자','수정주가비율','기관순매수량','기관누적순매수량'
+                    ,'등락주선','등락비율','예탁금','주식회전율','거래성립률']
+        list_ = ['시간','시가','고가','저가','종가','거래량','거래대금','누적체결매도수량'
+                 ,'등락주선','등락비율','예탁금','주식회전율','거래성립률']
             
-            stock_min_df.loc[v, input_list] = stock_day_df.loc[v, input_list].values
-        for code in concat_list:        
-            stock_min_df.to_csv('{0}concat_{1}_{2}.csv'.format(
-                            dir_concat_save, search_by_code(code)[0][1:],search_by_code(code)[1]
-                            ), encoding='utf-8-sig')           
-            
+        stock_min_df = stock_min_df.drop(input_list, axis=1)
+        stock_day_df = stock_day_df.drop(list_, axis=1)
+        
+        # if stock_min_df.index == stock_day_df.index
+        concat_df = pd.merge(stock_min_df, stock_day_df,on='Unnamed: 0')
+        
+        # concat_df = pd.concat([stock_min_df, stock_day_df], axis=1)
+        concat_df.to_csv(f'{dir_concat_save}concat_{min_df}', encoding='utf-8 sig')          
+             
             
 # 라벨링 처리
-def save_label_stock_info_param(file_concat, dir_concat_open, dir_label_save):
+def save_label_stock_info():
     
     for file_name in tqdm(files_concat):
         stock_info = pd.read_csv(f'{dir_concat_open}{file_name}', index_col=0)
