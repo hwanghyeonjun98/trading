58109a646d431387c4335eb5586cbd4c5bc27c9a
JJun <jjun1010@kakao.com>
2022-11-24 18:01:21 +0900

    웹관련

---
+++ web-dev/trading-web/src/main/java/com/big15/tradingweb/TradingWebApplication.java
@@ -0,0 +1,13 @@
+package com.big15.tradingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingWebApplication.class, args);
+	}
+
+}
---
+++ web-dev/trading-web/src/test/java/com/big15/tradingweb/TradingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big15.tradingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
