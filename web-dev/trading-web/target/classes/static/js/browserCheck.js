2348421b924443b848dfd539d60ec2bd408a381f
JJun <jjun1010@kakao.com>
2022-11-29 15:25:27 +0900

    -

--- web-dev/trading-web/target/classes/static/js/browserCheck.js
+++ web-dev/trading-web/target/classes/static/js/browserCheck.js
@@ -1,7 +1,7 @@
 const agent = navigator.userAgent.toLowerCase();
 
 if ((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1)) {
-	alert("익스플로러는 홈페이지 사용이 불가능합니다.\n크롬, 엣지, 웨일, 파이어폭스, 사파리 브라우저에서 접속해 주세요.");
+	alert("Internet Explorer는 호환되지 않는 브라우저 입니다.'");
 	location.href = "../error/browser_info.jsp";
 }
 console.log(agent);
