8fa6242650336ae5810675062e19a8cc699cd31b
JJun <jjun1010@kakao.com>
2022-12-13 12:33:17 +0900

    -

--- web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/Error.java
+++ web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/Error.java
@@ -19,7 +19,7 @@ public class Error implements ErrorController {
 	@RequestMapping(value = "/*")
 	public String handleError(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		log.info("error code : " + status.toString());
+		log.error("error code : " + status.toString());
 		if (status != null) {
 			int statusCode = Integer.valueOf(status.toString());
 			if (statusCode == HttpStatus.NOT_FOUND.value()) {
