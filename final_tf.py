76c39f80a38075081afa7b9c5ef2a021a352c001
KYW <big15@@@>
2022-12-14 18:26:14 +0900

    xzczx

--- final_tf.py
+++ final_tf.py
@@ -16,7 +16,7 @@ kospi_list, kosdaq_list, investing_df = get_target(path=r'D:\systrader-dev\tradi
 # # 대형주 1년 학습
 # ls_big_year = LstmNetwork('대형주', kospi_list, '1년', 280, 0.02)
 # col_list_big_year = ls_big_year.complete_corr
-# modelpath_big_year = ls_big_year
+# modelpath_big_year = ls_big_year.LstmModel()
 # model_big_year = load_model(modelpath_big_year)
 
 # # 대형주 1년 트레이딩
@@ -27,7 +27,7 @@ kospi_list, kosdaq_list, investing_df = get_target(path=r'D:\systrader-dev\tradi
 # # 대형주 6개월 학습
 # ls_big_six_month = LstmNetwork('대형주', kospi_list, '6개월', 140, 0.04 )
 # col_list_big_six_month = ls_big_six_month.complete_corr
-# modelpath_big_six_month = ls_big_six_month
+# modelpath_big_six_month = ls_big_six_month.LstmModel()
 # model_big_six_month = load_model(modelpath_big_six_month)
 
 # # 대형주 6개월 트레이딩
@@ -38,7 +38,7 @@ kospi_list, kosdaq_list, investing_df = get_target(path=r'D:\systrader-dev\tradi
 # # 소형주 1년 학습
 # ls_small_year = LstmNetwork('소형주', kosdaq_list, '1년', 280, 0.03)
 # col_list_small_year = ls_small_year.complete_corr
-# modelpath_small_year = ls_small_year
+# modelpath_small_year = ls_small_year.LstmModel()
 # model_small_year = load_model(modelpath_small_year)
 
 # # 소형주 1년 트레이딩
@@ -49,7 +49,7 @@ kospi_list, kosdaq_list, investing_df = get_target(path=r'D:\systrader-dev\tradi
 # 소형주 6개월 학습
 ls_small_six_month = LstmNetwork('소형주', kosdaq_list, '6개월', 140, 0.03)
 col_list_small_six_month = ls_small_six_month.complete_corr
-modelpath_small_six_month = ls_small_six_month
+modelpath_small_six_month = ls_small_six_month.LstmModel()
 model_small_six_month = load_model(modelpath_small_six_month)
 
 # 소형주 6개월 트레이딩
