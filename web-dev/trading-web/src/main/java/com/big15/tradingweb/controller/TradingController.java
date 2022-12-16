61de45e6eb8021836b984eb05664a5c0c40d8a3c
sjkim980609 <zldnltjdehd@naver.com>
2022-12-16 15:58:08 +0900

    --

--- web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/TradingController.java
+++ web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/TradingController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.security.KeyStore.Entry.Attribute;
 import java.util.List;
 import java.util.Locale;
 
@@ -31,14 +33,18 @@ public class TradingController {
 	// 브라우저 체크
 	// 브라우저가 인터넷 익스플로러면 다른 브라우저 접속, 다운로드 안내 페이지로 이동
 	@GetMapping("/")
-	public String browserCheck(HttpServletRequest request, Locale locale, Model model) {
+	public String Index(HttpServletRequest request, Locale locale, Model model) {
 		String userAgent = request.getHeader("User-Agent");
 
 		if (userAgent.indexOf("Trident") > -1) {
 			model.addAttribute("msg", "Internet Explorer는 호환되지 않는 브라우저 입니다.");
 			return "/errors/browser_info";
 		}
+		
+		List<InvestingDto> kospi_ = indexdataMapper.kospidata();
+		model.addAttribute("kospidata",kospi_);
 
+		
 		return "index";
 	}
 
@@ -57,10 +63,10 @@ public class TradingController {
 		return "/dataview/data";
 	}
 
-	@GetMapping("/")
+
 	public String maindata(Model model){
-		List<InvestingDto> kospi_ = indexdataMapper.kospidata();
-		return "/";
+		
+		return "index";
 	}
 
 }
