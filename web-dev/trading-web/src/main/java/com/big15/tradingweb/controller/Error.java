1370f1d604a716f474ac1b132ebf97c06d27e9fb
JJun <jjun1010@kakao.com>
2022-12-19 10:02:21 +0900

    -

--- web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/Error.java
+++ web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/Error.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
