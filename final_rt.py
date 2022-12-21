b73a4a182c01ab6a9a2fcdf47ce2bb4857dcf908
minkeon <vhjki999@gmail>
2022-12-21 11:58:49 +0900

    dsfs

--- final_rt.py
+++ final_rt.py
@@ -1,4 +1,4 @@
-from final_target import get_target_list_db
+from final_target import get_target_list_db_rt
 from final_realtime import realtime_trading
 
 from pandas.tseries.offsets import BDay
@@ -9,7 +9,7 @@ import pandas as pd
 yesterday=str(date.today() - BDay(1)).replace('-','').split(' ')[0]
 
 # 예측할 종목 설정
-kospi_list, kosdaq_list = get_target_list_db()
+kospi_list, kosdaq_list = get_target_list_db_rt()
 investing_df = pd.read_csv(f'./download/investing_df/investing_{yesterday}.csv', encoding='utf-8-sig', index_col=0)
 
 realtime_trading(kosdaq_list, investing_df)
