6dd14802b442dd0fe67e7c90d4c57d28722070e1
JJun <jjun1010@kakao.com>
2022-12-26 15:48:07 +0900

    회원가입 컨트롤러

---
+++ web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/JoinRestController.java
@@ -0,0 +1,9 @@
+package com.big15.tradingweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/join")
+public class JoinRestController {
+}
