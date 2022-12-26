a65ad69e385bafd2f13cd14597a615084b5f670f
JJun <jjun1010@kakao.com>
2022-12-26 17:11:36 +0900

    -

--- web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/JoinRestController.java
+++ web-dev/trading-web/src/main/java/com/big15/tradingweb/controller/JoinRestController.java
@@ -3,16 +3,14 @@ package com.big15.tradingweb.controller;
 import com.big15.tradingweb.dto.UserInfoDto;
 import com.big15.tradingweb.mapper.JoinMapper;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/join")
@@ -22,7 +20,12 @@ public class JoinRestController {
 
 
 	@PutMapping("/join")
-	public void join(@PathParam("user_id") String user_id, @PathParam("user_pw") String user_pw, @PathParam("user_name") String user_name, @PathParam("user_account") String user_account, HttpServletResponse response) throws IOException {
+	public void join(@RequestParam(value = "user_id") String user_id
+		, @RequestParam(value = "user_pw", required = false) String user_pw
+		, @RequestParam(value = "user_name", required = false) String user_name
+		, @RequestParam(value = "user_account", required = false) String user_account
+		, HttpServletResponse response) throws IOException {
+		log.info(user_id);
 		int join = joinMapper.join(user_id, user_pw, user_name, user_account);
 
 		if (join == 0) {
