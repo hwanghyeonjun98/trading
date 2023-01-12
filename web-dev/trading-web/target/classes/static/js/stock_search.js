059590fdb1bfccc5c24a3e1d1f169744fcc5ce46
JJun <jjun1010@kakao.com>
2023-01-12 12:27:42 +0900

    -

--- web-dev/trading-web/target/classes/static/js/stock_search.js
+++ web-dev/trading-web/target/classes/static/js/stock_search.js
@@ -2,8 +2,6 @@ let fsList = [];
 let fsInfo = [];
 
 const search = $("#fsData").val();
-const modalBtn = document.querySelector("#fsModalBtn");
-const modal = document.querySelector("#fsmodal");
 
 $(window).on("load", function () {
 	$.ajax({
@@ -43,59 +41,72 @@ $("#fsData").on("keyup", function (event) {
 $("#fsModalBtn").on("click", function () {
 	const search = $("#fsData").val();
 
-	if (fsInfo <= 0) {
-		$.ajax({
-			type     : "POST",
-			url      : "/api/data/fsInfo/" + search,
-			dataset  : "json",
-			success  : function (respense) {
-				$.each(respense, function (idx) {
-					str = "<tr>" +
-					      "<td>" + respense[idx].날짜 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_유동자산 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_비유동자산 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_자산총계 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_유동부채 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_비유동부채 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_부채총계 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_자본금 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_이익잉여금 + "</td>" +
-					      "<td>" + respense[idx].재무제표_재무상태표_자본총계 + "</td>" +
-					      "<td>" + respense[idx].재무제표_손익계산서_매출액 + "</td>" +
-					      "<td>" + respense[idx].재무제표_손익계산서_영업이익 + "</td>" +
-					      "<td>" + respense[idx].재무제표_손익계산서_법인세차감전_순이익 + "</td>" +
-					      "<td>" + respense[idx].재무제표_손익계산서_당기순이익 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_유동자산 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_비유동자산 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_자산총계 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_유동부채 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_비유동부채 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_부채총계 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_자본금 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_이익잉여금 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_재무상태표_자본총계 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_손익계산서_매출액 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_손익계산서_영업이익 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_손익계산서_법인세차감전_순이익 + "</td>" +
-					      "<td>" + respense[idx].연결재무제표_손익계산서_당기순이익 + "</td>" +
-					      "</tr>";
-					fsInfo.push(str);
-				});
-				$("#fsTable tbody").html();
-				return fsInfo;
-			},
-			complete : function () {
-				$(".loading-div").addClass("active");
-				$("#fsTable tbody").html(fsInfo);
-			},
-			error    : function () {
-				$("#fsTable tbody").html("<tr><td colspan='8'>정보가 없습니다.</td></tr>");
-			}
-		});
-	} else {
-		$(".loading-div").addClass("active");
-		$("#fsTable tbody").html("<tr><td colspan='8'>정보가 없습니다.</td></tr>");
+	if (search === "") {
+		alert("코드명을 입력해주세요.");
+		$("#fsTable tbody").html("<tr><td colspan='10'>정보가 없습니다.</td></tr>");
 	}
+
+	if ($("#fsTable tbody tr td").text() === "정보가 없습니다.") {
+		$(".loading-div").addClass("active");
+		$("#fsTable tbody").html("<tr><td colspan='10'>정보가 없습니다.</td></tr>");
+	}
+
+	$.ajax({
+		type     : "POST",
+		url      : "/api/data/fsInfo/" + search,
+		dataset  : "json",
+		success  : function (respense) {
+			fsInfo = []; // 배열 초기화
+			$.each(respense, function (idx) {
+				str = "<tr>" +
+				      "<td>" + respense[idx]._date + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_유동자산 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_비유동자산 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_자산총계 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_유동부채 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_비유동부채 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_부채총계 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_자본금 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_이익잉여금 + "</td>" +
+				      "<td>" + respense[idx].재무제표_재무상태표_자본총계 + "</td>" +
+				      "<td>" + respense[idx].재무제표_손익계산서_매출액 + "</td>" +
+				      "<td>" + respense[idx].재무제표_손익계산서_영업이익 + "</td>" +
+				      "<td>" + respense[idx].재무제표_손익계산서_법인세차감전_순이익 + "</td>" +
+				      "<td>" + respense[idx].재무제표_손익계산서_당기순이익 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_유동자산 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_비유동자산 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_자산총계 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_유동부채 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_비유동부채 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_부채총계 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_자본금 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_이익잉여금 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_재무상태표_자본총계 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_손익계산서_매출액 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_손익계산서_영업이익 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_손익계산서_법인세차감전_순이익 + "</td>" +
+				      "<td>" + respense[idx].연결재무제표_손익계산서_당기순이익 + "</td>" +
+				      "</tr>";
+				fsInfo.push(str);
+			});
+
+			$("#fsTable tbody").html();
+
+			return fsInfo;
+		},
+		complete : function () {
+			$(".loading-div").addClass("active");
+
+			if (fsInfo.length > 0) {
+				$("#fsTable tbody").html(fsInfo);
+			} else {
+				$("#fsTable tbody").html("<tr><td colspan='10'>정보가 없습니다.</td></tr>");
+			}
+		},
+		error    : function () {
+			$("#fsTable tbody").html("<tr><td colspan='10'>정보가 없습니다.</td></tr>");
+		}
+	});
 });
 
 $(".modal-close").on("click", function () {
